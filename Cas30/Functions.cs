351f497a5a3f1a2e3a9a2a23650ed0f143df1439
Violeta <violetapopans@gmail.com>
2020-12-25 21:42:21 +0100

    Addede class 30

---
+++ Cas30/Functions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas30
+{
+    class Functions
+    {
+        public static string RandomEmail()
+        {
+            Random Rnd = new Random();
+            string EmailSegment1, EmailSegment2, EmailSegment3;
+            EmailSegment1 = RandomAplphaNumeric(Rnd.Next(5 , 10)).ToLower();
+            EmailSegment1 += "." + RandomAplphaNumeric(Rnd.Next(5 , 10)).ToLower();
+            EmailSegment2 = RandomAplphaNumeric(Rnd.Next ( 7, 15)).ToLower();
+            EmailSegment3 = RandomAplpha(Rnd.Next(4, 6)).ToLower();
+
+            return EmailSegment1 + "@" + EmailSegment2 + "." + EmailSegment3;
+        }
+        public static string RandomTelephone()
+        {
+            Random Rnd = new Random();
+
+            StringBuilder Builder = new StringBuilder();
+            for (int i = 0; i < 12; i++)
+            {
+               if (i==3)
+                {
+                    Builder.Append("/");
+                } else if(i == 7) {
+                    Builder.Append("-");
+                } else
+                {
+                    Builder.Append(Rnd.Next(0, 9).ToString());
+                }
+            }
+            return Builder.ToString();
+        }
+        public static string RandomAplphaNumeric(int Length =10)
+        {
+            Random Rnd = new Random();
+            const string Pool = "asdfghjklmnbvcxzqwertyuiopASDFGHJKLMNBVCXZQWERTYUIOP1234567890";
+            var Builder = new StringBuilder();
+            for (int i = 0; i< Length; i++)
+            {
+                var Char = Pool[Rnd.Next(0, Pool.Length)];
+                Builder.Append(Char);
+            }
+            return Builder.ToString();
+        }
+        public static string RandomAplpha(int Length = 10)
+        {
+            Random Rnd = new Random();
+            const string Pool = "asdfghjklmnbvcxzqwertyuiopASDFGHJKLMNBVCXZQWERTYUIOP";
+            var Builder = new StringBuilder();
+            for (int i = 0; i < Length; i++)
+            {
+                var Char = Pool[Rnd.Next(0, Pool.Length)];
+                Builder.Append(Char);
+            }
+            return Builder.ToString();
+        }
+        public static int Random (int Min, int Max)
+        {
+            Random Rnd = new Random();
+            return Rnd.Next(Min, Max);
+        }
+    }
+
+}
--- Cas30/SeleniumTests.cs
+++ Cas30/SeleniumTests.cs
@@ -16,18 +16,82 @@ namespace Cas30
         [Category("Create"), Category("Site : test.qa.rs")]
         public void TestCreateNewUser()
         {
-            GoTo("https://test.qa.rs/");
+            GoTo("http://test.qa.rs/");
             IWebElement LinkCreate = Wait.Until
                 (EC.ElementIsVisible(By.LinkText("Kreiraj novog korisnika")));
             if(LinkCreate.Displayed && LinkCreate.Enabled)
             {
                 LinkCreate.Click();
             }
+            IWebElement InputName = Wait.Until(EC.ElementIsVisible(
+                By.Name("ime")));
+            if (InputName.Displayed && InputName.Enabled)
+            {
+                InputName.SendKeys(Functions.RandomAplpha(15 //Functions.Random(10 , 20)
+                    ));
+            }
+            IWebElement InputLastName = FindElement(By.Name("prezime"));
+            if(InputLastName != null)
+            { 
+                InputLastName.SendKeys("Pilipovic");
+            }
+            IWebElement InputUserName = FindElement(By.Name("korisnicko"));
+            InputUserName?.SendKeys(Functions.RandomAplphaNumeric(Functions.Random(10, 20)));
+
+            IWebElement InputEmail = FindElement(By.Name("email"));
+            InputEmail?.SendKeys(Functions.RandomEmail());
+
+            IWebElement InputPhone = FindElement(By.Name("telefon"));
+            InputPhone.SendKeys(Functions.RandomTelephone());
+
+            IWebElement InputCountry = FindElement(By.Name("zemlja"));
+            if(InputCountry != null)
+            {
+                SelectElement SelectCountry = new SelectElement(InputCountry);
+                SelectCountry.SelectByText("Sweden");
+                SelectCountry.SelectByValue("fin");
+                SelectCountry.SelectByIndex(6);
+            }
+            IWebElement InputCity = Wait.Until(EC.ElementIsVisible(By.Name("grad")));
+
+            if(InputCity.Displayed && InputCity.Enabled)
+            {
+                SelectElement SelectCity = new SelectElement(InputCity);
+                int NumberOfOptions = SelectCity.Options.Count;
+                Random Rnd = new Random ();
+                SelectCity.SelectByIndex(Rnd.Next(1, NumberOfOptions -1));
+            } 
+            IWebElement InputStreet = FindElement (By.XPath("//div[@id='address']//input"));
+            InputStreet.SendKeys(Functions.RandomAplpha(Functions.Random(10, 20)) + "" +
+                Functions.Random(1 , 100));
+
+            IWebElement InputGender = FindElement(By.Id("pol_m"));
+            InputGender.Click();
+
+            IWebElement ButonRegister = FindElement(By.Name("register"));
+            ButonRegister.Click();
             System.Threading.Thread.Sleep(5000);
+        }
+        public IWebElement FindElement(By Selector)
+        {
+            IWebElement ReturnElement = null;
+            try
+            {
+                ReturnElement = Driver.FindElement(Selector);
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+            return ReturnElement;
         }
          
         public void GoTo(string Url)
-        {
+        {  
             Driver.Navigate().GoToUrl(Url);
         }
 
@@ -41,7 +105,7 @@ namespace Cas30
         [TearDown]
         public void TearDown()
         {
-
+            Driver.Close();
         }
     }
 
